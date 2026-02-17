7ab47f665ad73b8ad45bcd743035139af6800f25
chcamp <cca40060138@gmail.com>
2026-02-17 01:18:39 -0500

    Hasta la clase 8 de ahi la 9

---
+++ quotes.py
@@ -0,0 +1,34 @@
+
+text = "Hola python"
+text2 = 'Hola , python'
+
+result  = text == text2
+print(result)
+
+message = 'Hola Andres'
+print(message)
+
+message = 'el codigo es del usuario'
+
+message2 = 'el codigo es del usuario\'s'
+print(message2)
+message2 = "el codigo es del usuario's"
+print(message2)
+
+text_multi_line = """
+    Esta es una
+    cadena de 
+    varias lineas
+    en 
+    Python
+    esto es muy util
+"""
+print(text_multi_line)
+
+#al capitulo 9 manejo de string
+
+
+
+
+
+
---
+++ type_python.py
@@ -0,0 +1,18 @@
+
+#tipo int sin declarar
+age = 40
+#tipo string sin declarar con comillas simples
+name = 'Christi'
+#print(f'Hola que tal {name} es {age}')
+print('Hola que tal '  + name  + ' es ' +  str(age))
+
+number_str = '30'
+number = int(number_str)
+print(50 - number)
+
+x = 10
+print(x)
+x = 'Hola que tal'
+print(x)
+
+
